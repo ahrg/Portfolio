d377bf592ae058db246a0d52e642601e86ff5b0f
ahrg <christofferv3@gmail.com>
2025-01-23 15:20:12 +0100

    t

--- src/AboutMeBox.js
+++ src/AboutMeBox.js
@@ -9,13 +9,6 @@ const AboutMeBox = () => {
                     <img src="/profile-picture.png" alt="profile-picture"></img>
                      </div>
                     <h1 className="Profile-Text"> Christoffer Schem</h1>
-                    <h1 className="Web-Dev Profile-Color">Web Developer</h1>
-                    <div className="Programming-Languages">
-                        <img src="./html.png" alt="Html"></img>
-                        <img src="css.png" alt="Css"></img>
-                        <img src="Javascript.png" alt="Javascript"></img>
-                        <img src="React.png" alt="React"></img>
-                    </div>
 
                     <div className="AboutMe-Contact-Container">
                         <h1 className="AboutMe-Contact">Email</h1>
@@ -25,6 +18,30 @@ const AboutMeBox = () => {
                         <h1>Phone number</h1>
                         <h1>+47 46152208</h1>
                     </div>
+                    <div className="AboutMe-Contact-Container">
+                        <h1>Github</h1>
+                        <h1>Click here</h1>
+                    </div>
+                    <div className="Programming-Languages-Container">
+                        <div className="Profile-Html">
+                            <img src="./html.png" alt="Html"></img>
+                            <h1 className="Red">Html</h1>
+                        </div>
+                        <div className="Profile-Css">
+                            <img src="css.png" alt="Css"></img>
+                            <h1 className="Blue">Css</h1>
+                        </div>
+                        <div className="Profile-Javascript">
+                            <img src="Javascript.png" alt="Javascript"></img>
+                            <h1 className="Orange">JS</h1>
+                        </div>
+                        <div className="Profile-React">
+                        <img src="React.png" alt="React"></img>
+                        <h1 className="React">React</h1>
+                        </div>
+                    </div>
+                    <h1 className="Web-Dev Profile-Color">Web Developer</h1>
+
                  </div>
              </div>
     )
